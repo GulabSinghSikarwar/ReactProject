65ee096b20801dce4ba692d9f2a58358cb1464ac
GulabSinghSikarwar <94665834+GulabSinghSikarwar@users.noreply.github.com>
2021-11-23 13:16:11 +0530

    ChangesTracker All previous SRc Files are added

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
+import {useState} from 'react';
+
 import './App.css';
+import ExpenseItems  from './ExpenseItems/ExpenseItems';
+import ExpenseForm  from './ExpenseItems/expenseForm/expenseForm';
 
 function App() {
+  const saveChangeHandeler=(data)=>{
+    console.log("Inside APP JS and Data is ")
+    console.log(data);
+
+  
+  }
+   const [title,setTitle]=useState()
+   const [date ,setDate]=useState()
+   const [price ,setPrice]=useState()
+
+   const  expense=[
+    {title:"MacBook pro ", price:"$2500",date:"30 march 2021"},
+    {title:"Investing  pro ", price:"$22500",date:"25 July 2021"},
+    {title:" I Phone", price:"$12500",date:"10 April 2021"},
+    {title:" I Pad ", price:"$2500",date:"20 march 2021"}
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +38,14 @@ function App() {
         >
           Learn React
         </a>
+    
+        <ExpenseForm onSaveChanges={saveChangeHandeler}/>
+        {expense.map(ele=><ExpenseItems title={ele.title} price={ele.price} date={ele.date}/>)}
+
+        {/* <ExpenseItems title ={expense[0].title} price={expense[0].price} date={expense[0].date} />
+        <ExpenseItems title ={expense[1].title} price={expense[1].price} date={expense[1].date} />
+        <ExpenseItems title ={expense[2].title} price={expense[2].price} date={expense[2].date} />
+        */}
       </header>
     </div>
   );
---
+++ expense-tracker/src/ExpenseItems/expenseForm/expenseForm.js
@@ -0,0 +1,68 @@
+import { React, useState } from "react"
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Button, Form } from 'react-bootstrap';
+
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("")
+  const [enteredPrice, setEnteredPrice] = useState("")
+  const [enteredDate, setenteredDate] = useState("")
+  const submitHandeler = (event) => {
+    event.preventDefault()
+    const newExpenses={
+      title:enteredTitle,
+      price:enteredPrice,
+      date:enteredDate
+    }
+    console.log(`Inside Form ${newExpenses}`)
+    console.log(newExpenses);
+    props.onSaveChanges(newExpenses)
+
+  }
+  const titleChangeHandeler=(event)=>{
+    setEnteredTitle(event.target.value)
+    console.log(enteredTitle );
+
+  }
+  const priceChangeHandeler=(event)=>{
+    setEnteredPrice(event.target.value)
+    console.log(enteredPrice );
+
+  }
+  const dateChangeHandeler=(event)=>{
+    setenteredDate(event.target.value)
+console.log(enteredDate);
+  }
+
+  return (
+    <div>
+
+      <Form onSubmit={submitHandeler} style={{ backgroundColor: "white", color: "black", padding: "25px", borderRadius: "5%" }}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Title </Form.Label>
+          <Form.Control type="text" onChange={titleChangeHandeler} placeholder="Enter Title " />
+          <Form.Text className="text-muted">
+            Please Enter the title of expenses
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Price </Form.Label>
+          <Form.Control type="number" onChange={priceChangeHandeler} placeholder="Enter Price " />
+        </Form.Group>
+
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Date </Form.Label>
+          <Form.Control type="date"  onChange={dateChangeHandeler} placeholder="Enter Price " />
+        </Form.Group>
+
+
+        <Button variant="primary" type="submit" onSubmit={submitHandeler}>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  )
+}
+export default ExpenseForm;
