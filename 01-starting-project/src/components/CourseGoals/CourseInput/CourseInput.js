48bad8c16e59d7ec400d575197ad4ac3345d6024
GulabSinghSikarwar <94665834+GulabSinghSikarwar@users.noreply.github.com>
2021-11-24 14:21:28 +0530

    Starting Project To Safe Zone

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -4,22 +4,38 @@ import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 const CourseInput = props => {
+
   const [enteredValue, setEnteredValue] = useState('');
+   const [isValid,setIsValid]=useState(true)
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
+    if(event.target.value.trim().length>0)
+    {
+        setIsValid(true)
+    }
+
+
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+
+      if(enteredValue.trim().length===0){
+          setIsValid(false)
+          return
+      }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label
+            style={{color:isValid?"black":"maroon"}}>Course Goal</label>
+        <input type="text"
+         style={{backgroundColor:!isValid?"salmon":"transparent"}}
+               onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
