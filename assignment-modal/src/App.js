50f1d2fe8ad986c83f15a0b9265f758017b68bb0
GulabSinghSikarwar <94665834+GulabSinghSikarwar@users.noreply.github.com>
2021-11-27 16:08:56 +0530

    Added Basic Design for modals

--- assignment-modal/src/App.js
+++ assignment-modal/src/App.js
@@ -7,17 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div  style={{margin:"0px  0px 25px 0px" ,fontWeight:"bold",textTransform:"Uppercase"}}>
+
+          WelCome Users
+        </div>
+
         <Container/>
       </header>
     </div>
--- assignment-modal/src/components/InputForm/inputForm.js
+++ assignment-modal/src/components/InputForm/inputForm.js
@@ -1,5 +1,6 @@
 
 import React  from "react";
+import  './InputForm.css'
 
 const  InputForm=()=>{
 
@@ -7,10 +8,14 @@ const  InputForm=()=>{
         <div className="form-control">
             <div className="form-control-elements">
                 <label>Name :</label>
+                <input type="text" name={"Name"}/>
+
 
             </div>
             <div className="form-control-elements">
                 <label>Age:</label>
+                <input type={"number"} name={"Age"}/>
+
 
             </div>
 
